814e00098021541d7a1e78b2fbc3ac89af874019
mvilgelm <mikhail.vilgelm@tum.de>
2016-01-29 12:50:15 +0100

    upd basic model

--- wtm/basic.py
+++ wtm/basic.py
@@ -5,50 +5,92 @@ import math, os
 import matplotlib.pyplot as plt
 import matplotlib
 
-matplotlib.rcParams.update({'font.size': 14})
-matplotlib.rcParams.update({'figure.autolayout': True})
 
-# saloha - throughput
+# saloha - throughput (utilization)
 def t_aloha(arr):
     return arr*math.exp(-arr)
+
 
 # saloha - idle probability
 def i_aloha(arr):
     return math.exp(-arr)
 
+
 # saloha - collision probability
 def e_aloha(arr):
     return 1.0 - t_aloha(arr) - i_aloha(arr)
 
+
+# tdma - throughput (utilization)
 def t_tdma(arr):
     if arr>1:
         return 1
     return arr
 
-plt.figure(figsize=(7, 4.5))
 
-load = [0.01+0.01*x for x in range(400)]
+def plot_saloha_vs_tdma(load_range):
 
-# throughput
-plt.plot(load, [t_aloha(x) for x in load], '-b')
+    # configure plot parameters
+    matplotlib.rcParams.update({'font.size': 14})
+    matplotlib.rcParams.update({'figure.autolayout': True})
+    plt.figure(figsize=(7, 4.5))
 
-# collisions
-plt.plot(load, [i_aloha(x) for x in load], '-.r')
+    load = load_range
 
-# idle
-plt.plot(load, [e_aloha(x) for x in load], '--g')
+    # saloha
+    plt.plot(load, [t_aloha(x) for x in load], '-b')
+
+    # tdma
+    plt.plot(load, [t_tdma(x) for x in load], '-g')
+
+    # display parameters
+    plt.grid(True)
+    plt.ylim((0, 1.1))
+    plt.xlabel('Normalized load '+r'$\lambda$')
+    plt.ylabel('Resource utilization '+r'$T$')
+
+    plt.legend(('C.-based', 'C.-free'), loc=0)
+
+    # by default --> show but not save
+    plt.show()
+    # plt.savefig(os.getenv("HOME")+'/Dropbox/_lkn/basic.png', format='png', bbox='tight')
 
 
-# plt.ylim((0, 1.1))
-plt.grid(True)
-plt.xlabel('Normalized load '+r'$\lambda$')
-# plt.ylabel('Resource utilization '+r'$T$')
-# plt.ylabel('Throughput '+r'$T$')
-plt.ylabel('Probability')
+def plot_saloha_performance(load_range):
 
-plt.legend(('success', 'collision', 'idle'), loc=0)
-# plt.legend(('Contention-free' + r'$T = \lambda \exp^{-\lambda}$', r'$T = \lambda$'), loc=0)
-# plt.legend(('C.-based', 'C.-free'), loc=0)
+    # configure plot parameters
+    matplotlib.rcParams.update({'font.size': 14})
+    matplotlib.rcParams.update({'figure.autolayout': True})
+    plt.figure(figsize=(7, 4.5))
 
-# plt.show()
-plt.savefig(os.getenv("HOME")+'/Dropbox/_lkn/basic.png', format='png', bbox='tight')
+    load = load_range
+
+    # throughput
+    plt.plot(load, [t_aloha(x) for x in load], '-b')
+
+    # collisions
+    plt.plot(load, [e_aloha(x) for x in load], '-.r')
+
+    # idle
+    plt.plot(load, [i_aloha(x) for x in load], '--g')
+
+
+    # plt.ylim((0, 1.1))
+    plt.grid(True)
+    plt.xlabel('Normalized load '+r'$\lambda$')
+    plt.ylabel('Probability')
+
+    plt.legend(('success', 'collision', 'idle'), loc=0)
+
+    # by default: only show
+    plt.show()
+    # plt.savefig(os.getenv("HOME")+'/Dropbox/_lkn/basic.png', format='png', bbox='tight')
+
+
+if __name__=='__main__':
+
+    load = [0.01+0.01*x for x in range(400)]
+
+    # plot_saloha_vs_tdma(load)
+
+    plot_saloha_performance(load)
